11c148e5a12bcb82dd8fb30560a2e7e26c8c1693
Serhiei <Serhieievb@gmail.com>
2024-05-30 21:59:03 +0300

    feat: alias

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   server: { watch: { usePolling: true } },
   plugins: [react()],
+  resolve: {
+    alias: {
+      src: './src',
+      components: './src/components',
+      pages: './src/pages',
+      assets: './src/assets',
+    },
+  },
   base: '/quizz-testTask/',
   build: {
     outDir: 'dist',
