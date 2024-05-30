a2ce685bc8008a81a35adb3bd8ab0209694b3606
Serhiei <Serhieievb@gmail.com>
2024-05-30 21:41:24 +0300

    fix: issue was founded in basename

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/quiz-testTask">
+      <BrowserRouter basename="/quizz-testTask/">
         <PersistGate loading={null} persistor={persistor}>
           <App />
         </PersistGate>
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  server: { watch: { usePolling: true } },
   plugins: [react()],
-  resolve: {
-    alias: {
-      src: '/src',
-      components: '/src/components',
-      pages: '/src/pages',
-      assets: '/src/assets',
-    },
-  },
-  base: '/quiz-testTask',
+  base: '/quizz-testTask/',
   build: {
     outDir: 'dist',
     assetsDir: '.',
